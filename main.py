659ddb27ccc8e8f89f77778dbb6f3c5799827a7e
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2025-09-28 09:52:13 +0300

    добавил main добавил загрузку переменных из окружения

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from telegram import Bot
+from dotenv import load_dotenv
+
+
+from os import getenv
+from asyncio import run
+
+
+async def main():
+    load_dotenv()
+    token = getenv("TOKEN")
+    Bot(token)
 
 
 if __name__ == "__main__":
-    pass
+    run(main())
